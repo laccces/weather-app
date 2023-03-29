8352072ad6730d5c1f07dff3cc55885cc3fb1fea
laccces <en07djc@icloud.com>
2023-03-29 11:19:41 +0100

    Built out call template.

---
+++ weather.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+# Enter your API key here
+api_key = "f27bbe94a88d423fa13100714232903"
+
+# Base URL for the API
+base_url = "http://api.weatherapi.com/v1/forecast.json?"
+
+# Enter the city name
+city_name = input("Enter city name : ")
+
+days = input("How many days of weather would you like? Range allowed is 1-10.")
+
+# Complete URL for the API request
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+# Sending HTTP request
+response = requests.get(complete_url)
+
+# Parsing the response
+data = response.json()
+
+with open('data.json', 'w') as file:
+    json.dump(data, file, indent=4)
+
+# Extracting necessary info
+# temperature = data['main']['temp']
+# humidity = data['main']['humidity']
+# description = data['weather'][0]['description']
+
+# Displaying the data
+# print("Temperature : " +
+#                     str(round(temperature - 273.15, 2)) + " Celsius")
+# print("Humidity : " +
+#                     str(humidity) + "%")
+# print("Description : " +
+#                     str(description))
