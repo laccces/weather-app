55e5864a23c1416d2523c0b2b2a725c1c7f41e11
laccces <en07djc@icloud.com>
2023-03-29 11:32:11 +0100

    Fixed api call

--- weather.py
+++ weather.py
@@ -8,12 +8,12 @@ api_key = "f27bbe94a88d423fa13100714232903"
 base_url = "http://api.weatherapi.com/v1/forecast.json?"
 
 # Enter the city name
-city_name = input("Enter city name : ")
+city_name = input("Enter city name: ")
 
 days = input("How many days of weather would you like? Range allowed is 1-10.")
 
 # Complete URL for the API request
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+complete_url = base_url + "key=" + api_key + "&q=" + city_name + "&days=" + days + "&aqi=no&alerts=no"
 
 # Sending HTTP request
 response = requests.get(complete_url)
